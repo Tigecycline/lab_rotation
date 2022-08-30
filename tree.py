bf4bfae9ba029a47439fb84e83b1c656c8045668
Xiaoyu <binggans@126.com>
2022-08-30 08:58:55 +0200

    Tree visualization

--- tree.py
+++ tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+import graphviz
 from tree_node import TreeNode
 
 
@@ -8,7 +9,7 @@ class CellTree:
     def __init__(self, likelihoods1, likelihoods2, gt1, gt2, reversible = None): 
         self.L = likelihoods1.shape[0] # number of leaves (observed cells)
         self.M = likelihoods1.shape[1] # number of mutations
-        self.nodes = [TreeNode(i) for i in range(self.N)]
+        self.nodes = [TreeNode(i) for i in range(self.N)] # TBC: use dictionary instead
         self.gt1 = gt1 
         self.gt2 = gt2 
         self.L1 = np.sum(likelihoods1, axis = 0) # log-likelihood all cells have gt1
@@ -17,7 +18,7 @@ class CellTree:
         self.LLR[:self.L,:] = likelihoods2 - likelihoods1
         
         if reversible is None: # if reversibility not given, consider all mutations as irreversible
-            self.reversible = [False for i in range(self.M)] 
+            self.reversible = [False] * self.M 
         else: 
             self.reversible = reversible 
         
@@ -37,25 +38,65 @@ class CellTree:
         return self.nodes[-1]
     
     
+    def create_random_subtree(self, nodes_idx, new_idx): 
+        '''
+        Create a random binary tree on selected nodes 
+        nodes_idx: indices of the selected nodes 
+        new_idx: nodes from this index are used as internal nodes of the subtree
+        '''
+        remaining = nodes_idx # indices of nodes without parent
+        parent_idx = new_idx
+        for i in range(len(nodes_idx) - 1): 
+            children_idx = np.random.choice(remaining, size = 2, replace = False)
+            # TBC: clear existing children of the parent 
+            self.nodes[children_idx[0]].assign_parent(self.nodes[parent_idx])
+            self.nodes[children_idx[1]].assign_parent(self.nodes[parent_idx])
+            remaining.remove(children_idx[0])
+            remaining.remove(children_idx[1])
+            remaining.append(parent_idx)
+            parent_idx += 1
+    
+    
+    def read_mutation_tree(self, mutation_tree): 
+        '''
+        Works only when the mutation tree is properly sorted, i.e. children nodes are found after their parents (which should be the case if the mutation tree is generated using the default constructor)
+        '''
+        mutation_nodes = mutation_tree.nodes.copy()
+        leaves = [np.where(mutation_tree.attachments == i)[0].tolist() for i in range(self.L)] # attached cells & other internal nodes
+        print(leaves)
+        
+        idx = self.L
+        while mutation_nodes: 
+            node = mutation_nodes.pop() # unprocessed node with highest index, all its children should be processed already
+            print('Processing node', node.name)
+            print('The leaves are:', leaves[node.name])
+            n_leaves = len(leaves[node.name])
+            print(node)
+            if n_leaves == 0: 
+                pass
+            elif n_leaves == 1: 
+                leaves[node.parent.name].append(leaves[node.name][0]) # pass the child to its grandparent
+            else: 
+                self.create_random_subtree(leaves[node.name], idx)
+                if not node.isroot: 
+                    subtree_root = idx + n_leaves - 2
+                    leaves[node.parent.name].append(subtree_root)
+                    idx = subtree_root + 1 
+        self.nodes[-2].assign_parent(self.root)
+    
+    
     def refresh_edges(self, mutation_tree = None): 
         if mutation_tree is None: # random tree if no mutation tree is provided
-            remaining = [i for i in range(self.L)] # indices of nodes without parent
-            for i in range(self.L, self.N - 1): 
-                idx = np.random.choice(remaining, size = 2, replace = False)
-                self.nodes[i].add_child(self.nodes[idx[0]]) # or leave it as numpy array?
-                self.nodes[i].add_child(self.nodes[idx[1]]) 
-                remaining.remove(idx[0])
-                remaining.remove(idx[1])
-                remaining.append(i)
-            self.root.add_child(self.nodes[-2])
+            self.create_random_subtree([i for i in range(self.L)], self.L)
+            self.nodes[-2].assign_parent(self.root)
         else: 
-            pass # TO BE IMPLEMENTED
+            self.read_mutation_tree(mutation_tree)
     
     
     def refresh_LLR(self, node): 
         if not node.isleaf: # nothing to be done for leaves
-            self.LLR[node.name,:] = 0 # erase old LLR (if exists)
-            for child in node.get_children(): # DF traverse of all descendants
+            self.LLR[node.name,:] = 0 # erase old LLR 
+            for child in node.children: 
                 self.refresh_LLR(child)
                 self.LLR[node.name,:] += self.LLR[child.name,:]
     
@@ -66,10 +107,17 @@ class CellTree:
         best_nodes = np.argmax(self.LLR, axis = 0)
         for i in range(self.M): 
             self.nodes[best_nodes[i]].add_mutation(i)
-            
     
-    def read_mutation_tree(self, mutation_tree): 
-        pass
+    
+    def move_subtree(self, mrca_idx, target_idx): 
+        # Todo: test this function
+        assert(mrca_idx < self.N - 1 and target_idx < self.N - 1)
+        mrca = self.nodes[mrca_idx]
+        target = self.nodes[target_idx]
+        assert(mrca is not self.root and mrca is not target)
+        assert(not target.descends_from(mrca))
+        
+        
     
     
     def node_info(self): 
@@ -77,15 +125,48 @@ class CellTree:
             print(node)
     
     
-    def structure_info(self): 
+    def to_graphviz(self, filename = None, show = False): 
+        dg = graphviz.Digraph(filename = filename)
         for node in self.nodes: 
-            print(node.name, '-->', [child.name for child in node.get_children()])
+            if node.isleaf: 
+                dg.node(str(node.name), shape = 'circle')
+            else: 
+                dg.node(str(node.name), label = '', shape = 'circle')
+            for child in node.children: 
+                label = ', '.join(['m' + str(m) for m in child.mutations])
+                dg.edge(str(node.name), str(child.name), label = label)
+        if show: 
+            dg.view()
+        return dg
 
 
 
 
 class MutationTree:     
-    def __init__(self, likelihoods1, likelihoods2, cell_tree = None): 
+    def __init__(self, likelihoods1 = None, likelihoods2 = None, cell_tree = None): 
+        # \begin {for test}
+        if likelihoods1 is None: 
+            self.L = 8
+            
+            self.nodes = [TreeNode(i) for i in range(7)]
+            self.nodes[1].assign_parent(self.nodes[0])
+            self.nodes[2].assign_parent(self.nodes[0])
+            self.nodes[3].assign_parent(self.nodes[0])
+            self.nodes[4].assign_parent(self.nodes[1])
+            self.nodes[5].assign_parent(self.nodes[2])
+            self.nodes[6].assign_parent(self.nodes[2])
+            
+            self.nodes[1].mutations = [0,1]
+            self.nodes[2].mutations = [2]
+            self.nodes[3].mutations = [3]
+            self.nodes[4].mutations = [4]
+            self.nodes[5].mutations = [5]
+            self.nodes[6].mutations = [6]
+            
+            self.attachments = np.array([1,1,0,5,5,5,3,2], dtype = int)
+            return
+            
+        # \end {for test}
         self.L = likelihoods1.shape[0] # number of leaves (observed cells)
         self.M = likelihoods1.shape[1] # number of mutations
         
@@ -130,7 +211,7 @@ class MutationTree:
         self.likelihoods[:,0] = np.sum(likelihoods1, axis = 1) # root has no mutation
         # The construction of the tree ensures that, in self.nodes, children are always found after their parents. So we can traverse the nodes using the order in self.nodes. 
         for node in self.nodes[1:]: 
-            self.likelihoods[:,node.name] = self.likelihoods[:,node.get_parent().name] + np.sum(sm_LLR[:,node.mutations], axis = 1)
+            self.likelihoods[:,node.name] = self.likelihoods[:,node.parent.name] + np.sum(sm_LLR[:,node.mutations], axis = 1)
         
     
     def attach_cells(self): 
@@ -145,7 +226,23 @@ class MutationTree:
     
     def attachment_info(self): 
         print(self.attachments)
+        
     
+    def to_graphviz(self, filename = None, show = False): 
+        dg = graphviz.Digraph(filename = filename, engine = 'neato')
+        for node in self.nodes: 
+            label = ', '.join(['m' + str(m) for m in node.mutations])
+            dg.node(str(node.name), label = label, shape = 'rectangle')
+            for child in node.children:
+                dg.edge(str(node.name), str(child.name))
+        for i in range(self.L): 
+            cell_name = 'cell' + str(i)
+            dg.node(cell_name, label = str(i), shape = 'plaintext')
+            parent = self.nodes[self.attachments[i]]
+            dg.edge(str(parent.name), cell_name)
+        if show: 
+            dg.view()
+        return dg
     
     
     
@@ -165,7 +262,7 @@ if __name__ == '__main__':
     
     #ref = pd.read_csv('./Data/glioblastoma_BT_S2/ref.csv', index_col = 0).to_numpy(dtype = float)
     #alt = pd.read_csv('./Data/glioblastoma_BT_S2/alt.csv', index_col = 0).to_numpy(dtype = float)
-    
+    '''
     likelihoods1 = np.log(np.array([[1/2, 1/2], 
                                     [1/4, 1/2], 
                                     [1/4, 1/4]]))
@@ -181,8 +278,33 @@ if __name__ == '__main__':
     ct.node_info()
     
     print('**********************************')
-    mt = MutationTree(likelihoods1, likelihoods2, ct)
+    mt = MutationTree(likelihoods1, likelihoods2, ct)b
     mt.node_info()
     mt.attachment_info()
     print()
     
+    print('**********************************')
+    ct.read_mutation_tree(mt)
+    ct.node_info()
+    '''
+    
+    mt = MutationTree()
+    
+    likelihoods1 = np.log(np.array([[1/2, 1/2], 
+                                    [1/4, 1/2], 
+                                    [1/4, 1/4]]))
+
+    likelihoods2 = np.log(np.array([[1/4, 1/4], 
+                                    [1/2, 1/4], 
+                                    [1/2, 1/2]]))
+    
+    gt1 = ['H', 'H']
+    gt2 = ['R', 'R']
+    
+    ct = CellTree(likelihoods1, likelihoods2, gt1, gt2)
+    
+    ct.nodes = [TreeNode(i) for i in range(16)]
+    ct.L = 8
+    ct.read_mutation_tree(mt)
+    
+    ct.node_info()
--- tree_node.py
+++ tree_node.py
@@ -5,11 +5,11 @@ class TreeNode:
         else: 
             self.name = name
         
-        self._parent = None
+        self.parent = None
         if parent is not None: 
             self.assign_parent(parent)
         
-        self._children = []
+        self.children = []
         if children is not None: 
             self.add_children(children)
         
@@ -21,12 +21,12 @@ class TreeNode:
     
     @property
     def isleaf(self): 
-        return self._children == []
+        return self.children == []
     
     
     @property
     def isroot(self): 
-        return self._parent is None
+        return self.parent is None
     
     
     @property
@@ -44,36 +44,45 @@ class TreeNode:
         Tests equality of two subtrees without considering node names
         If used after self.sort, tests isomorphism of the two subtrees
         '''
-        return self.mutations == other.mutations and self._children == other._children
+        return self.mutations == other.mutations and self.children == other.children
     
     
     def __str__(self): 
         result = ''
-        result += '****** node ' + str(self.name) + ' ******\n'
-        if not self.isroot: 
-            result += 'parent: ' + str(self._parent.name) + '\n'
-        else: 
+        result += '****** node "' + str(self.name) + '" ******\n'
+        if self.isroot: 
             result += 'has no parent\n' 
-        if not self.isleaf: 
-            result += 'children: ' + str([child.name for child in self._children]) + '\n'
         else: 
+            result += 'parent: ' + str(self.parent.name) + '\n'
+        if self.isleaf: 
             result += 'has no children\n'
+        else: 
+            result += 'children: ' + str([child.name for child in self.children]) + '\n'
         result += 'mutations: ' + str(self.mutations) + '\n'
         return result
     
     
+    def descends_from(self, other): 
+        if self.parent is None: 
+            return False
+        elif self.parent is other or self.parent.descends_from(other): 
+            return True
+        else: 
+            return False
+    
+    
     def sort(self): 
         ''' Sorts the children and mutations of self and all its descendants '''
-        self._children.sort() 
+        self.children.sort() 
         self.mutations.sort() 
-        for child in self._children: 
+        for child in self.children: 
             child.sort() 
     
     
     def assign_parent(self, new_parent): 
-        old_parent = self._parent
-        self._parent = new_parent
-        new_parent._children.append(self)
+        old_parent = self.parent
+        self.parent = new_parent
+        new_parent.children.append(self)
         if old_parent is not None: 
             old_parent.remove_child(self)
     
@@ -88,19 +97,14 @@ class TreeNode:
     
     
     def remove_child(self, child): 
-        for i in range(len(self._children)): 
-            if self._children[i] is child: 
-                del self._children[i] 
+        for i in range(len(self.children)): 
+            if self.children[i] is child: 
+                del self.children[i] 
                 break
-        # TBC: warning when child is not in self._children
+        # TBC: warning when child is not in self.children
         #print('*****************************************************')
         #print('[TreeNode.remove_child] WARNING: Child doesn\'t exist.')
         #print('*****************************************************')
-    
-    
-    def swap_parents(self, other): 
-        self.assign_parent(other._parent)
-        other.assign_parent(self._parent)
     
     
     #def df_apply(self, func): 
@@ -108,14 +112,6 @@ class TreeNode:
     #    func(self)
     #    for child in self.children: 
     #        child.df_apply(func)
-    
-    
-    def get_parent(self): 
-        return self._parent
-    
-    
-    def get_children(self): # TBC: hide children and implement a function to traverse tree
-        return self._children
     
     
     def get_nonempty_descendants(self, container = None): 
@@ -127,7 +123,7 @@ class TreeNode:
         if container is None: 
             container = []
         
-        for child in self._children: 
+        for child in self.children: 
             if child.isempty: 
                 child.get_nonempty_descendants(container)
             else: 
@@ -163,3 +159,13 @@ class TreeNode:
         new_node = TreeNode(name, parent, children)
         new_node.mutations = self.mutations
         return new_node
+
+
+
+
+def random_binary_tree(nodes): 
+    '''
+    Create a random binary tree on selected nodes 
+    '''
+    # nodes without a parent
+    nodes = nodes + [TreeNodes]
